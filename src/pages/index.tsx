2165dbeb22c2a8c7640f5f82f33a6b8f65cb967a
Arturo <arturo.conti.2004@gmail.com>
2025-08-07 17:29:35 +0200

    finished fr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,17 +22,16 @@ export default function Home() {
                 Hi, I'm ArtuCN and this is my page
               </Descriptions.Item>
               <Descriptions.Item label="How I made it">
-                I have spent like 8 hours to finish this thing
+                I have spent like 10 hours to finish this thing and probably I'm never gonna touch this again 
               </Descriptions.Item>
               <Descriptions.Item label="Do I like it?">
-                Absolutely not, it is disgusting, but it was few time and it was my first time with NodeJS
+                Absolutely not, it is disgusting, but it was few time and it was my first time with NodeJS and there wasn't a lot of cool things to use in this Api
               </Descriptions.Item>
               <Descriptions.Item label="Do I like Node?">
                 Yes, probably it is the best front-end framework I have ever used
               </Descriptions.Item>
               <Descriptions.Item label="Final comment">
-                I just want to say that I hate this API, it hasn't images and there are few information.
-                If you think that I am the problem you are a bad person, so I had to create the worst page ever!
+                I don't like it but if you don't like it you are a bad person, if you think that you are so better than me try you to create something cool with ah not uopdated-api by using a framework for the first time in like hours
               </Descriptions.Item>
             </Descriptions>
           </div>
